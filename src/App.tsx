a9730136f49ce72825ab25863734add9cc278c73
msienkiewicz7 <msienkiewicz@disroot.org>
2021-06-10 16:39:31 +0200

    App logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState, MouseEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QuestionCard from './components/QuestionCard';
+import { Difficulty, fetchQuizQuestions, QuestionState } from './API';
+import Score from './components/Score';
 
-function App() {
+
+
+const TOTAL_QUESTIONS = 10
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [gameOver, setGameOver] = useState(true);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnwsers, setUserAnwsers] = useState([]);
+  const [score, setScore] = useState(0);
+
+
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+    const questions = await fetchQuizQuestions(10, Difficulty.HARD)
+    setQuestions(questions)
+  }
+
+  useEffect(() => {
+    setLoading(false)
+  }, [questions])
+
+  const checkAnwser = (e: MouseEvent) => {
+
+  }
+
+  const nextQuestion = (e: MouseEvent<HTMLButtonElement>) => {
+    setNumber(number+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>REACT QUIZ</h1>
+      <button className="start-btn" onClick={startTrivia}>
+        Start
+      </button>
+
+      {!gameOver && <Score score={score} /> }
+
+      {loading &&
+        <p>Loading Questions ...</p>
+      }
+
+      {questions[number] &&
+        <QuestionCard
+          anwsers={questions ? questions[number].anwsers : []}
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          userAnwser={userAnwsers ? userAnwsers[number] : undefined}
+          callback={checkAnwser}
+        />
+      }
+
+
+      {questions[number+1] && 
+        <button className="next-btn" onClick={nextQuestion}> 
+          Next question
+        </button>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
